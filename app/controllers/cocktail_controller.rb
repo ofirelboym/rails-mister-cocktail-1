1a938d2f3018d8d1d3100257fb096e5dfb02cf88
Dean Wheeler <dfwheeler394@gmail.com>
2019-12-17 18:38:29 -0500

    modified, refactored cocktails controller

--- app/controllers/cocktail_controller.rb
+++ app/controllers/cocktail_controller.rb
@@ -1,12 +1,10 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
-    @dose = Dose.new
-    @review = Review.new
   end
 
   def new
@@ -15,16 +13,18 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    if @cocktail.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      render 'new'
-    end
+    @cocktail.save
+
+    redirect_to '/'
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
