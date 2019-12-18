398bb015709c93f0fa1250a6d8e9e8364113b85e
Dean Wheeler <dfwheeler394@gmail.com>
2019-12-17 19:16:46 -0500

    routes and schema updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  resources :cocktails do
-    resources :doses, only: [:create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :cocktails, except: :index do
+    resources :doses, only: [:new, :create, :destroy]
   end
-  resources :doses, only: [:destroy]
 end
